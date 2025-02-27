43115c53590b4fe859e3c6cb12f09677eff047fa
iizukar <czechia.deitan@gmail.com>
2025-02-27 23:13:32 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python3 server.py &
 
 # Pipe the acceptance confirmation to Honeygain in case it waits for interactive input
-printf "%s\n" "-tou-accept" | honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME"
+honeygain -tou-accept -email tamakagi.deitan@gmail.com -pass ryota20020219 -device tamakagi
