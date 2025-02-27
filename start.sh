954a6d6a05304f49bfbafabefc2ea4d96777179d
iizukar <czechia.deitan@gmail.com>
2025-02-28 00:32:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# Start the fake HTTP server in the background
-python3 server.py &
 
 #terms
 honeygain -tou-get
@@ -8,3 +6,9 @@ honeygain -tou-get
 # Run Honeygain with your command-line parameters.
 # Replace the placeholder environment variables with your actual Render environment variables.
 honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME"
+
+#accept
+-tou-accept
+
+# Start the fake HTTP server in the background
+python3 server.py &
