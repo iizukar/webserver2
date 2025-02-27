f0b2c1116b081824a346099d6eb7befd1205e8eb
iizukar <czechia.deitan@gmail.com>
2025-02-28 00:10:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Step 1: Retrieve the TOU (this can be skipped if already done)
 # Use the full path to the honeygain binary
-/honeygain/honeygain -tou-get
+honeygain/honeygain -tou-get
 
 # Step 2: Accept the TOU with your credentials.
 # Running it in the background allows the script to continue.
- /honeygain/honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
+honeygain/honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
 
 # (Optional) Wait a few seconds to let Honeygain initialize
 sleep 5
