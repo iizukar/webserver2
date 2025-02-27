4d9bcc535b4b35fb67a3e6c843d642cd0de8e996
iizukar <czechia.deitan@gmail.com>
2025-02-27 22:43:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ python3 server.py &
 
 # Run Honeygain with your command-line parameters.
 # Replace the placeholder environment variables with your actual Render environment variables.
-honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME"
+honeygain -tou-accept -email $ACCOUNT_EMAIL -pass $ACCOUNT_PASSWORD -device $DEVICE_NAME
