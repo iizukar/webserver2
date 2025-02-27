2c9e3b9c3b99f4e4be71bedc0b32cbb6deba2ed7
iizukar <czechia.deitan@gmail.com>
2025-02-27 23:44:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-# Start the fake HTTP server in the background
+# Start the fake HTTP server in the background to satisfy Render's requirement.
 python3 server.py &
 
-# Run the Expect script to launch Honeygain and handle interactive prompt
-expect accept_terms.exp
+# pull
+honeygain/honeygain
+
+
+# Start Honeygain as per the official Docker instructions.
+honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME"
