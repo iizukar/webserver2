b4f691b709559c76d24738a472132b7363fa94a0
iizukar <czechia.deitan@gmail.com>
2025-02-28 01:48:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Start Honeygain
-honeygain -tou-get
-honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
+./honeygain -tou-get
+./honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
 
 # Start dummy HTTP server on port 8000
 python3 -m http.server 8000 --bind 0.0.0.0 &
