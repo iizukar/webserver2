d8988131e78b83c61e7f0a3f981b72a62951c308
iizukar <czechia.deitan@gmail.com>
2025-02-28 16:47:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 PORT=${PORT:-8000}
 
-# Add Cargo to PATH
-export PATH="/root/.cargo/bin:$PATH"
-
-# Start bore tunnel for Honeygain's port
-bore local 19321 --port 19321 &
+# Start Localtonet tunnel for Honeygain's port (replace YOUR_FREE_KEY)
+localtonet -key YOUR_LOCALTONET_KEY -tcp 19321 &
 
 # Start Honeygain
 ./honeygain -tou-get
 ./honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
 
-# Dummy server for Render
+# Dummy HTTP server for Render
 python3 -m http.server $PORT --bind 0.0.0.0 &
 
+# Keep alive
 tail -f /dev/null
