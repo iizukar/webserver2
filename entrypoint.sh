afadfd8e683afb4daee6f7f1d8baf2b748284a4f
iizukar <czechia.deitan@gmail.com>
2025-02-28 16:57:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 PORT=${PORT:-8000}
 
-# Start Localtonet tunnel for Honeygain's port (replace YOUR_FREE_KEY)
-localtonet -key YOUR_LOCALTONET_KEY -tcp 19321 &
+# Start Localtonet tunnel (replace YOUR_TOKEN)
+localtonet -token $LOCALTONET_TOKEN -tcp 19321 &
 
 # Start Honeygain
 ./honeygain -tou-get
 ./honeygain -tou-accept -email "$ACCOUNT_EMAIL" -pass "$ACCOUNT_PASSWORD" -device "$DEVICE_NAME" &
 
-# Dummy HTTP server for Render
+# Dummy HTTP server
 python3 -m http.server $PORT --bind 0.0.0.0 &
 
-# Keep alive
+# Keep container alive
 tail -f /dev/null
