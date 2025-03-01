02a77992dbb3c8e776b1f4995ecaed6d02b3247d
iizukar <czechia.deitan@gmail.com>
2025-03-01 02:00:25 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,7 @@ python3 -m http.server 8000 --directory /dummy &
 # Login to Bitping with MFA
 /app/bitpingd login \
   --email "$BITPING_EMAIL" \
-  --password "$BITPING_PASSWORD" \
-  --mfa "$BITPING_MFA"
+  --password "$BITPING_PASSWORD" 
 
 # Start main Bitpingd process
 exec /app/bitpingd "$@"
